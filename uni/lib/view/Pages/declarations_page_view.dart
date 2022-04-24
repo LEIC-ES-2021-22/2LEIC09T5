d582c73ba2bba5da932ba7441b6caba396b7afad
Nuno Pereira <nunoafonso2002@gmail.com>
2022-04-24 15:10:23 +0100

    Added title to declarations page

--- uni/lib/view/Pages/declarations_page_view.dart
+++ uni/lib/view/Pages/declarations_page_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
+import 'package:uni/view/Widgets/declarations_page_title.dart';
 
 class DeclarationsPageView extends StatefulWidget {
   @override
@@ -8,6 +9,24 @@ class DeclarationsPageView extends StatefulWidget {
 
 class DeclarationsPageViewState extends SecondaryPageViewState {
 
-
+  @override
+  Widget getBody(BuildContext context) {
+  
+    return ListView(
+      children: <Widget>[
+        Container(
+          child: Column(
+            children: <Widget>[
+              DeclarationsPageTitle(),
+              Text("testData1"),
+              Text("testData2"),
+              Text("testData3"),
+            ]
+          )
+        ),
+      ],
+    );
+  }
 
 }
+
