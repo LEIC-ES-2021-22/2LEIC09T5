4049e46d8cbd57095060923d3593c15224a7bc1c
Nuno Pereira <nunoafonso2002@gmail.com>
2022-04-25 04:22:30 +0100

    Added declaration card

--- uni/lib/view/Pages/declarations_page_view.dart
+++ uni/lib/view/Pages/declarations_page_view.dart
@@ -9,22 +9,38 @@ class DeclarationsPageView extends StatefulWidget {
 }
 
 class DeclarationsPageViewState extends SecondaryPageViewState {
+  final double borderRadius = 12;
+
   @override
   Widget getBody(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Container(child: Column(children: this.buildChildren(context))),
+        DeclarationsPageTitle(),
+        this.buildDeclarationCard(context)
       ],
     );
   }
 
+  Widget buildDeclarationCard(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(5),
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(this.borderRadius),
+          color: Color.fromARGB(255, 245, 245, 245),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Color.fromARGB(255, 200, 200, 200),
+                blurRadius: this.borderRadius / 2)
+          ],
+        ),
+        child: Column(children: this.buildChildren(context)));
+  }
+
   List<Widget> buildChildren(BuildContext context) {
     return <Widget>[
-      DeclarationsPageTitle(),
       DeclarationRectangle("This is some sample DECLARATION text1"),
       DeclarationRectangle("This is some sample DECLARATION text2"),
-      DeclarationRectangle("This is some sample DECLARATION text3"),
-      DeclarationRectangle("This is some sample DECLARATION text4"),
     ];
   }
 }
