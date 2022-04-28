835ba61bc0c7e8e0353e4e9b5595dae246feac07
toino <joaoapereira21@hotmail.com>
2022-04-28 15:07:09 +0100

    Start working on certificates page

---
+++ uni/lib/view/Pages/certificates_page_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:uni/view/Pages/secondary_page_view.dart';
+
+class CertificatesPageView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CertificatesPageViewState();
+}
+
+class CertificatesPageViewState extends SecondaryPageViewState {}
