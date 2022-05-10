15bc8ee31afa05913a88a8e21b525f3490369758
PogfLux <luisosorio2001@gmail.com>
2022-05-10 15:04:04 +0100

    Started working on absence justifications

---
+++ uni/lib/view/Pages/absence_justification_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:uni/view/Pages/secondary_page_view.dart';
+
+class AbsenceJustificationPageView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    AbsenceJustificationPageViewState();
+  }
+}
+
+class AbsenceJustificationPageViewState extends SecondaryPageViewState {
+  
+}
