18c49c3abff60c8112c2d6464f990e3d2bd5c70a
PogfLux <luisosorio2001@gmail.com>
2022-05-10 15:04:04 +0100

    Started working on absence justifications

--- uni/lib/utils/constants.dart
+++ uni/lib/utils/constants.dart
@@ -4,11 +4,25 @@ const navPersonalArea = 'Área Pessoal';
 const navSchedule = 'Horário';
 const navExams = 'Mapa de Exames';
 const navDeclarations = "Declarações";
+const navAbsences = 'Justificação de Faltas';
 const navStops = 'Autocarros';
 const navAbout = 'Sobre';
 const navBugReport = 'Bugs e Sugestões';
 const navLogOut = 'Terminar sessão';
 
-const faculties = ['faup', 'fbaup', 'fcup', 'fcnaup', 'fadeup',
-                  'fdup', 'fep', 'feup', 'ffup', 'flup', 'fmup',
-                  'fmdup', 'fpceup', 'icbas'];
+const faculties = [
+  'faup',
+  'fbaup',
+  'fcup',
+  'fcnaup',
+  'fadeup',
+  'fdup',
+  'fep',
+  'feup',
+  'ffup',
+  'flup',
+  'fmup',
+  'fmdup',
+  'fpceup',
+  'icbas'
+];
