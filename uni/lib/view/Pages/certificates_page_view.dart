5b5fe7e8aabd041cd91136595dbc06091e02379f
toni-santos <a.oliveirasantos122@gmail.com>
2022-05-12 17:54:17 +0100

    Updated certificates page to display cards

--- uni/lib/view/Pages/certificates_page_view.dart
+++ uni/lib/view/Pages/certificates_page_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
 import 'package:uni/view/Widgets/certificates_page_title.dart';
+import 'package:uni/view/Widgets/certification_card.dart';
 
 class CertificatesPageView extends StatefulWidget {
   @override
@@ -8,24 +9,20 @@ class CertificatesPageView extends StatefulWidget {
 }
 
 class CertificatesPageViewState extends SecondaryPageViewState {
+  final double borderRadius = 12;
 
   @override
   Widget getBody(BuildContext context) {
-
     return ListView(
       children: <Widget>[
         Container(
-          child: Column(
-            children: <Widget>[
-              CertificatesPageTitle(),
-              Text("testData21"),
-              Text("testData2"),
-              Text("testData3"),
-            ]
-          )
-        ),
+            child: Column(children: <Widget>[
+          CertificatesPageTitle(),
+          CertificationCard("Certificado 1", Status.accepted, "24/1/22"),
+          CertificationCard("Certificado 2", Status.pending, "2/1/22"),
+          CertificationCard("Certificado 3", Status.rejected, "4/2/21"),
+        ])),
       ],
     );
   }
-
 }
