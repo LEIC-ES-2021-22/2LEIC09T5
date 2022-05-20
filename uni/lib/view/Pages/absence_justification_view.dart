c280bb8995349c1a8b3c2de06dde8239fdf227d4
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-05-20 15:44:42 +0100

    Justification page with title

--- uni/lib/view/Pages/absence_justification_view.dart
+++ uni/lib/view/Pages/absence_justification_view.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
+import 'package:uni/view/Widgets/absence_justification_page_title.dart';
+import 'package:uni/view/Pages/request_Justification_page_view.dart';
 
 class AbsenceJustificationPageView extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    AbsenceJustificationPageViewState();
-  }
+  State<StatefulWidget> createState() => AbsenceJustificationPageViewState();
 }
 
 class AbsenceJustificationPageViewState extends SecondaryPageViewState {
-  
+  @override
+  Widget getBody(BuildContext context) {
+
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                AbsenceJustificationPageTitle(),
+                
+              ]
+            )
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => RequestJustificationPageView())),
+      ),
+    );
+  }
 }
