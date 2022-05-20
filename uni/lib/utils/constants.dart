cfd0e509f3b688c8998c79dd715649d456b829ff
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-05-20 15:09:45 +0100

    constants

--- uni/lib/utils/constants.dart
+++ uni/lib/utils/constants.dart
@@ -3,14 +3,8 @@ library Constants;
 const navPersonalArea = 'Área Pessoal';
 const navSchedule = 'Horário';
 const navExams = 'Mapa de Exames';
-<<<<<<< HEAD
 const navCertificates = 'Certificados';
 const navDeclarations = "Declarações";
-<<<<<<< HEAD
-=======
->>>>>>> 4f04473 (Started working on absence justifications)
-=======
->>>>>>> 1843e70299dc1dc6d9755026a13cd090be50da74
 const navAbsences = 'Justificação de Faltas';
 const navStops = 'Autocarros';
 const navAbout = 'Sobre';
