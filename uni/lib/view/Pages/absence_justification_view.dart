b68bbff4707080992b8127d926e9784d6bc15973
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-05-22 01:08:30 +0100

    Added justification cards

--- uni/lib/view/Pages/absence_justification_view.dart
+++ uni/lib/view/Pages/absence_justification_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
 import 'package:uni/view/Widgets/absence_justification_page_title.dart';
 import 'package:uni/view/Pages/request_Justification_page_view.dart';
+import 'package:uni/view/Widgets/justification_card.dart';
 
 class AbsenceJustificationPageView extends StatefulWidget {
   @override
@@ -19,7 +20,9 @@ class AbsenceJustificationPageViewState extends SecondaryPageViewState {
             child: Column(
               children: <Widget>[
                 AbsenceJustificationPageTitle(),
-                
+                JustificationCard("L.EIC", "ESOF", Status.accepted, "24/1/22"),
+                JustificationCard("L.EIC", "ESOF", Status.pending, "2/1/22"),
+                JustificationCard("L.EIC", "ESOF", Status.rejected, "4/2/21"),
               ]
             )
           ),
