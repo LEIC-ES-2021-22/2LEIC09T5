7dd10dd91a23d65928e72d4b62f389b6275811d8
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-05-27 15:52:40 +0100

    add justification form

--- uni/lib/view/Pages/request_justification_page_view.dart
+++ uni/lib/view/Pages/request_justification_page_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:uni/model/app_state.dart';
 import 'package:uni/view/Pages/unnamed_page_view.dart';
 import 'package:uni/view/Widgets/absence_justification_page_title.dart';
+import 'package:uni/view/Widgets/request_justification_form.dart';
 
 class RequestJustificationPageView extends StatefulWidget {
   const RequestJustificationPageView({Key key}) : super(key: key);
@@ -27,7 +28,7 @@ class RequestJustificationsPageViewState extends UnnamedPageView {
       child: ListView(
         children: [
           AbsenceJustificationPageTitle(),
-          //RequestCertificatesForm(),
+          RequestJustificationForm(),
         ],
       ),
     );
