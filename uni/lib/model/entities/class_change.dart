b8494711fb5b627236b2913f0de4e41ce018f12e
Nuno Pereira <nunoafonso2002@gmail.com>
2022-06-07 01:14:29 +0100

    Implemented class change secondary page

---
+++ uni/lib/model/entities/class_change.dart
@@ -0,0 +1,9 @@
+class ClassChange {
+  final String studentName;
+  final int studentNumber;
+  final String unitName;
+  final String prevClass, newClass;
+
+  ClassChange(String this.studentName, int this.studentNumber,
+      String this.unitName, String this.prevClass, String this.newClass);
+}
--- uni/lib/view/Pages/class_change_page_view.dart
+++ uni/lib/view/Pages/class_change_page_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
 import 'package:uni/view/Widgets/class_change_page_title.dart';
-import 'package:uni/view/Widgets/declaration_rectangle.dart';
-import 'package:uni/controller/declarations/declarations_fetcher.dart';
-import 'package:uni/model/entities/declaration.dart';
+import 'package:uni/view/Widgets/class_change_card.dart';
+import 'package:uni/model/entities/class_change.dart';
+import 'package:uni/view/Pages/request_class_change_page_view.dart';
 
 class ClassChangePageView extends StatefulWidget {
   @override
@@ -25,11 +25,44 @@ class ClassChangePageContent extends StatefulWidget {
 class ClassChangePageContentState extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ClassChangePageTitle(),
-        // this.buildDeclarationsCard(context),
-      ],
+    final children = [
+      // these are just for testing purposes
+      ClassChangeCard(ClassChange(
+          'Nuno Afonso Anjos Pereira',
+          202007865,
+          'Laboratório de Desenho e Teste de Software',
+          '2L.EIC01',
+          '2L.EIC02')),
+      ClassChangeCard(ClassChange('Nuno Afonso Anjos Pereira', 202007865,
+          'Algoritmos e Estruturas de Dados', '2L.EIC09', '2L.EIC08')),
+      ClassChangeCard(ClassChange('Nuno Afonso Anjos Pereira', 202007865,
+          'Física 2', '2L.EIC12', '2L.EIC13')),
+      ClassChangeCard(ClassChange('Nuno Afonso Anjos Pereira', 202007865,
+          'Programação', '2L.EIC12', '2L.EIC13')),
+    ];
+
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          ClassChangePageTitle(),
+          ListView.separated(
+            padding: EdgeInsets.only(bottom: 20),
+            itemCount: children.length,
+            itemBuilder: (context, i) => children[i],
+            separatorBuilder: (context, i) =>
+                SizedBox.fromSize(size: Size.fromHeight(10)),
+            shrinkWrap: true,
+            primary: false,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => RequestClassChangePageView())),
+      ),
     );
   }
 }
